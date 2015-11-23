dee48cea093b0ebf89622747c817c6d4f74fd838
kwdoyle <cheeze13579@gmail.com>
2015-11-22 22:39:28 -0500

    found taxon namespace problem in calculating distances between two trees

--- calc_distance_Hirak.py
+++ calc_distance_Hirak.py
@@ -1,15 +1,17 @@
 import dendropy
 
-T_H = dendropy.Tree(stream=open("ph3.nre"), schema="newick")
-T_DMC = dendropy.Tree(stream=open("ph_dmc.nre"), schema="newick")
-T_seq = dendropy.Tree(stream=open("ph_seq.nre"), schema="newick")
-T_F = dendropy.Tree(stream=open("ph3_DNA_F.nre"), schema="newick")
+T_H = dendropy.Tree.get(file=open("ph1.nre", "r"), schema="newick")
+T_DMC = dendropy.Tree.get(file=open("ph_dmc.nre", "r"), schema="newick")
+T_seq = dendropy.Tree.get(file=open("ph_seq.nre", "r"), schema="newick")
+T_F = dendropy.Tree.get(file=open("ph1_DNA_F.nre", "r"), schema="newick")
 
 
 print "Distance between T_H with T_DMC " + str(T_H.robinson_foulds_distance(T_DMC))
 print "Distance between T_H with T_seq " + str(T_H.robinson_foulds_distance(T_seq))
 print "Distance between T_H with T_F " + str(T_H.robinson_foulds_distance(T_F))
 
+# Calculating these distances gives an error about a non-identical taxon namespace between the two trees.
+# This most likely means that the taxons assigned to each tree literally need to be the same object.
 
 print "Distance between T_H with T_DMC " + str(T_H.symmetric_difference(T_DMC))
 print "Distance between T_H with T_seq " + str(T_H.symmetric_difference(T_seq))
