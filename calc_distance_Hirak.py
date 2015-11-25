bedca6928088b363cb8658f6a91b93c389e793ef
kwdoyle <cheeze13579@gmail.com>
2015-11-25 13:00:35 -0500

    calculated stats of the other trees

--- calc_distance_Hirak.py
+++ calc_distance_Hirak.py
@@ -6,35 +6,38 @@ T_DMC = dendropy.Tree.get(file=open("ph_dmc.nre", "r"), schema="newick")
 T_seq = dendropy.Tree.get(file=open("ph_seq.nre", "r"), schema="newick")
 T_F = dendropy.Tree.get(file=open("ph6_DNA_F.nre", "r"), schema="newick")
 
-# Make taxon namespaces
+# Turn tree files into strings
+T_H_string = str(T_H) + ";"
 T_DMC_string = str(T_DMC) + ";"
 T_seq_string = str(T_seq) + ";"
+T_F_string = str(T_F) + ";"
 
-# testing
-tree_list1 = dendropy.TreeList()
-tree_list1.read(data=T_DMC_string, schema="newick")
-tree_list2 = dendropy.TreeList(taxon_namespace=tree_list1.taxon_namespace)
-tree_list2.read(data=T_seq_string, schema="newick")
+# Make taxon namespaces using those strings
+T_H_list = dendropy.TreeList()
+T_H_list.read(data=T_H_string, schema="newick") # T_H_string (was T_DMC_string)
 
-# Results in: 6
-print(treecompare.symmetric_difference(tree_list1[0], tree_list2[0]))
+T_DMC_list = dendropy.TreeList(taxon_namespace=T_H_list.taxon_namespace)
+T_DMC_list.read(data=T_DMC_string, schema="newick") # T_DMC_string (was T_seq_string)
 
-# so this creation of the namespaces works, as these two tree_lists below will print out the contents of
-# the original tree files, AND the symmetric_difference function above also successfully runs.
-#print tree_list1[0]
-#print tree_list2[0]
+T_seq_list = dendropy.TreeList(taxon_namespace=T_H_list.taxon_namespace)
+T_seq_list.read(data=T_seq_string, schema="newick") # T_seq_string (was T_H_string)
 
-# Calculating the robinson foulds distance will now work with this method too!!!
-print(treecompare.weighted_robinson_foulds_distance(tree_list1[0], tree_list2[0]))
+T_F_list = dendropy.TreeList(taxon_namespace=T_H_list.taxon_namespace)
+T_F_list.read(data=T_F_string, schema="newick") # T_F_string
 
 
-#print "Distance between T_H with T_DMC " + str(treecompare.weighted_robinson_foulds_distance(T_H, T_DMC))#T_H.robinson_foulds_distance(T_DMC))
-#print "Distance between T_H with T_seq " + str(T_H.robinson_foulds_distance(T_seq))
-#print "Distance between T_H with T_F " + str(T_H.robinson_foulds_distance(T_F))
+# so this creation of the namespaces works, as these two lists below will print out the contents of
+# the original tree files, AND the statistical functions below also successfully run.
+#print T_H_list[0]
+#print T_DMC_list[0]
 
-# Calculating these distances gives an error about a non-identical taxon namespace between the two trees.
-# This most likely means that the taxons assigned to each tree literally need to be the same object.
+# Calculating the robinson foulds distances
+print "Distance between T_H and T_DMC: " + str(treecompare.weighted_robinson_foulds_distance(T_H_list[0], T_DMC_list[0]))
+print "Distance between T_H and T_seq: " + str(treecompare.weighted_robinson_foulds_distance(T_H_list[0], T_seq_list[0]))
+print "Distance between T_H and T_F: " + str(treecompare.weighted_robinson_foulds_distance(T_H_list[0], T_F_list[0]))
 
-#print "Distance between T_H with T_DMC " + str(T_H.symmetric_difference(T_DMC))
-#print "Distance between T_H with T_seq " + str(T_H.symmetric_difference(T_seq))
-#print "Distance between T_H with T_F " + str(T_H.symmetric_difference(T_F))
+
+# Calculating symmetric differences
+print "Symmetric difference between T_H and T_DMC: " + str(treecompare.symmetric_difference(T_H_list[0], T_DMC_list[0]))
+print "Symmetric difference between T_H and T_seq: " + str(treecompare.symmetric_difference(T_H_list[0], T_seq_list[0]))
+print "Symmetric difference between T_H with T_F: " + str(treecompare.symmetric_difference(T_H_list[0], T_F_list[0]))
