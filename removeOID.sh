2d26e0b85e765f5b826a48c853a5f2c2debac6ec
kwdoyle <cheeze13579@gmail.com>
2015-11-30 13:57:10 -0500

    had to make new file be different than the input file, otherwise the new file is blank

--- removeOID.sh
+++ removeOID.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat $1 | sed -e 's/43.\{8\}//g' > $1
+cat $1 | sed -e 's/45.\{8\}//g' > $1_rm_oid.nre
 # need to change the first 2 numbers of the ID to match that of the file in question
 # this uses the sed command to search the file for "43" (or whatever the first 2 numbers
 # of the object ID are) and then deletes the next 8 characters that follow it.
