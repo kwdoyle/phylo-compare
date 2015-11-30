053e2c3d11145fc781e1ed71c4ab6c0ff085ea7f
kwdoyle <cheeze13579@gmail.com>
2015-11-30 13:49:30 -0500

    forgot to make removeOID rewrite the contents of the tree file

--- removeOID.sh
+++ removeOID.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cat $1 | sed -e 's/43.\{8\}//g'
+cat $1 | sed -e 's/43.\{8\}//g' > $1
 # need to change the first 2 numbers of the ID to match that of the file in question
+# this uses the sed command to search the file for "43" (or whatever the first 2 numbers
+# of the object ID are) and then deletes the next 8 characters that follow it.
 
 # Usage: ./removeOID <file.nre>
